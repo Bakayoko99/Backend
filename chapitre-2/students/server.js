6d2f8f0b4c3a84c721b53724f3c2e9bde2acef50
Bakagnan <cr7sendu@gmail.com>
2021-05-26 17:33:03 +0200

    exo students

---
+++ chapitre-2/students/server.js
@@ -0,0 +1,39 @@
+
+const express = require("express")
+const cors = require("cors")
+
+const app = express()
+
+app.use(express.json())
+
+app.use(cors())
+
+var port = 8099;
+
+
+const studentsName = ["amir", "mugilan", "asma", "karim", "faruk"]
+
+app.get('/students', (req, res) => {
+    res.json(
+        studentsName
+    )
+})
+
+app.post('/students',(req, res) => {
+
+    const newName = req.body.name
+
+    studentsName.push(newName)
+
+    console.log(newName);
+
+    
+})
+
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port: ' + port);
+  });
+  
